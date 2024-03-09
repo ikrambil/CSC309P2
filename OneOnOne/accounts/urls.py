90cd72ea89d9f8c33a6d9b28bb0a6eebaddd2d1a
nidaa-7 <nidaa.rabih.rabah7@gmail.com>
2024-03-08 19:12:11 -0500

    used RESTful API

--- OneOnOne/accounts/urls.py
+++ OneOnOne/accounts/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name = 'register'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('profile/edit/', views.EditView.as_view(), name='edit'),
-    path('profile/view/', views.ViewView.as_view(), name='view_profile'),
-    path('logout/', views.LogoutView.as_view(), name = 'logout'),
+    path('register/', views.RegisterAPIView.as_view(), name = 'register'),
+    path('login/', views.LoginAPIView.as_view(), name='login'),
+    path('profile/edit/', views.EditProfileAPIView.as_view(), name='edit'),
+    path('profile/view/', views.ViewProfileAPIView.as_view(), name='view_profile'),
+    path('logout/', views.LogoutAPIView.as_view(), name = 'logout'),
 ]
