bbfdbc8bf553f461a9d642dc597e8bd93e181c74
nidaa-7 <nidaa.rabih.rabah7@gmail.com>
2024-03-13 04:06:16 -0400

    added docs.pdf, startup.sh, run.sh

---
+++ startup.sh
@@ -0,0 +1,17 @@
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install required Python packages
+pip install django djangorestframework python-dateutil
+
+cd OneOnOne
+
+# Run migrations 
+python manage.py makemigrations
+python manage.py migrate
