2895b9d8379821066bcbfd10c14dcfe4c0939e52
Taeuk Kang <taeukkang09@gmail.com>
2024-03-28 13:37:29 -0400

    Modify shell

--- OneOnOne/startup.sh
+++ OneOnOne/startup.sh
@@ -2,13 +2,14 @@
 python3 -m venv venv
 
 # Activate the virtual environment
-source myvenv/bin/activate
+source venv/bin/activate
 
 # Upgrade pip
 pip install --upgrade pip
 
 # Install required Python packages
 pip install django djangorestframework python-dateutil
+pip install djangorestframework-simplejwt
 
 # Run migrations 
 python manage.py makemigrations
