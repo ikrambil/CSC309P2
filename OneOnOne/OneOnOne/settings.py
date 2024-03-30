da9c79ce4034af8986e155effc0f240146e33e44
Bilal Ikram <bilal0106ikram@gmail.com>
2024-03-30 00:53:47 -0400

    Package updates

--- OneOnOne/OneOnOne/settings.py
+++ OneOnOne/OneOnOne/settings.py
@@ -71,7 +71,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'OneOnOne.urls'
 
