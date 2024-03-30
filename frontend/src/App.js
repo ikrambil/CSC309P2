c6b4fffc740d8c45db53c5931d7a5427d62fa8bb
Bilal Ikram <bilal0106ikram@gmail.com>
2024-03-30 00:53:24 -0400

    css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import CreateCalendar from './components/CreateCalendar';
+import './styles.css';
 import './App.css';
+import 'flowbite'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes> {/* Use Routes instead of Switch */}
+          <Route path="/dashboard/:userId" element={<Dashboard />} />
+          <Route path="/create-calendar" element={<CreateCalendar />} />
+          {/* Define other routes here */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
