f586279429134598ca9fbee08ac56203b3d72ab8
Taeuk Kang <taeukkang09@gmail.com>
2024-03-31 14:29:14 -0400

    Changed to hosted url

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -41,7 +41,7 @@ function Login() {
 
     try {
       const response = await fetch(
-        "http://127.0.0.1:8000/accounts/login/",
+        "https://csc309p2.onrender.com/accounts/login/",
         {
           method: "POST",
           headers: {
